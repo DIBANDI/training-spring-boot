b683833e93d67b3cda8bce0b6cc8f3e7c27aea32
DIBANDI <ngolarols@gmail.com>
2020-10-26 11:24:14 +0100

    Spring boot security partie 8

--- MicrocommerceApplication/src/main/java/com/ecommerce/microcommerce/configuration/ProductSecurityConfig.java
+++ MicrocommerceApplication/src/main/java/com/ecommerce/microcommerce/configuration/ProductSecurityConfig.java
@@ -1,4 +1,42 @@
 package com.ecommerce.microcommerce.configuration;
 
-public class ProductSecurityConfig {
+        import org.springframework.context.annotation.Bean;
+        import org.springframework.context.annotation.Configuration;
+        import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+        import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+        import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+        import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+@Configuration
+public class ProductSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("rols").password("password").roles("ADMIN");
+        auth.inMemoryAuthentication().withUser("troli").password("password2").roles("USER");
+
+    }
+
+    //securite par des roles utilisateur
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+
+                .authorizeRequests()
+                .antMatchers("/Produits/*").hasRole("ADMIN")
+                .antMatchers("/Produits/test/{prix}").hasRole("USER")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+
+    }
+
+    @Bean
+    public static NoOpPasswordEncoder passwordEncoder(){
+        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+    }
 }
+
