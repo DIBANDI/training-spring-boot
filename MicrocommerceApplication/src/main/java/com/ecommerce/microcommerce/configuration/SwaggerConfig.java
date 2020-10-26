7a8022b963d129d2af04c777e58a243d47bbcc33
DIBANDI <ngolarols@gmail.com>
2020-10-26 11:21:45 +0100

    Swagger partie 10

--- MicrocommerceApplication/src/main/java/com/ecommerce/microcommerce/configuration/SwaggerConfig.java
+++ MicrocommerceApplication/src/main/java/com/ecommerce/microcommerce/configuration/SwaggerConfig.java
@@ -2,7 +2,9 @@ package com.ecommerce.microcommerce.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -12,30 +14,37 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.Collection;
+import java.util.Collections;
 
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
+@EnableWebSecurity
+public class SwaggerConfig extends WebMvcConfigurerAdapter  {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.ecommerce.microcommerce.web"))
                 .paths(PathSelectors.regex("/Produits.*"))
-                .build();
+                .build()
+                .apiInfo(apiDetails());
     }
 
-    private ApiInfo metaData() {
-        return new ApiInfoBuilder()
-                .title("Spring Boot REST API")
-                .description("\"Spring Boot REST API for Online Store\"")
-                .version("1.0.0")
-                .license("Apache License Version 2.0")
-                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
-                .contact(new Contact("Rols NGOLA", "https://rolsngola.com", "ngolarols@gmail.com"))
-                .build();
+    private ApiInfo apiDetails() {
+        return new ApiInfo(
+                "Spring Boot REST API",
+                "Ce Projet m'a permis de developper mes competences en Spring Boot ",
+                "1.0.0",
+                "Apache License Version 2.0",
+                new Contact("Rols NGOLA", "https://rolsngola.com", "ngolarols@gmail.com"),
+                "API licence",
+                "https://rolsngola.com",
+                Collections.emptyList());
     }
+
+@Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
